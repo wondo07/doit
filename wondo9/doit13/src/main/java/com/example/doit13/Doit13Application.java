7b39a9cd0801d8a7b8e56f314e0f3545c98874ff
wondo07 <110904783+wondo07@users.noreply.github.com>
2022-12-21 00:04:58 +0900

    doit13

---
+++ wondo9/doit13/src/main/java/com/example/doit13/Doit13Application.java
@@ -0,0 +1,13 @@
+package com.example.doit13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Doit13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Doit13Application.class, args);
+	}
+
+}
